5f2917e1b2456f840e8307c9a6b55dc4d22a83be
colin campbell <colincampbelldesigns@outlook.com>
2021-01-06 11:44:14 +0000

    Bittersweet Tattoo website
    Progress made so far on bittersweet Tattoo website, still be be completed

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('number');
+const message = document.getElementById('message');
+const inputs = document.getElementsByClassName('form__input');
+
+const form = document.getElementById('form');
+
