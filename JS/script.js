9643e3b0ee1ca17a6dd75ad4f6bec43a66d80945
colin campbell <colincampbelldesigns@outlook.com>
2021-01-18 10:40:55 +0000

    new images
    New images added and edited for the imag gallery

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 
+const nav = document.querySelector('nav');
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const phone = document.getElementById('number');
@@ -6,4 +7,13 @@ const message = document.getElementById('message');
 const inputs = document.getElementsByClassName('form__input');
 
 const form = document.getElementById('form');
+
+
+const backgroundFunction = window.addEventListener('scroll', () => {
+    if(nav.clientTop !== 0){
+        nav.classList.add('scroll')
+    }
+})
+
+
 
