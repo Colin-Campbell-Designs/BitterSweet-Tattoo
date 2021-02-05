fde21fe4d66879be828fe117f8c3b14de2192b4b
colin campbell <colincampbelldesigns@outlook.com>
2021-02-05 06:46:37 +0000

    new updates to gallery and footer
    new images added to gallery and also the footer being built out

--- JS/script.js
+++ JS/script.js
@@ -15,5 +15,36 @@ const backgroundFunction = window.addEventListener('scroll', () => {
     }
 })
 
+var map;
 
+function initMap() {
+    const bittersweet = { lat: 54.62468707449994, lng: -5.9754263423425 };
+     map = new google.maps.Map(document.getElementById("map"), {
+      center: bittersweet,
+      zoom: 17,
+    });
+    const marker = new google.maps.Marker({
+        position: bittersweet,
+        map: map,
+    })
+  }
 
+  
+
+  
+ 
+
+//   function initMap() {
+//     // The location of Uluru
+//     const uluru = { lat: -25.344, lng: 131.036 };
+//     // The map, centered at Uluru
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//       zoom: 4,
+//       center: uluru,
+//     });
+//     // The marker, positioned at Uluru
+//     const marker = new google.maps.Marker({
+//       position: uluru,
+//       map: map,
+//     });
+//   }
